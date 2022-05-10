200fab9102cfea48809ca731178b54faf27ee05a
wukaijun <1172917145@qq.com>
2022-05-10 22:13:13 +0800

    object

---
+++ Ignore/src/java/p2/ui/CustomerView.java
@@ -0,0 +1,12 @@
+package src.java.p2.ui;
+
+import src.java.p2.service.CustomerList;
+
+/**
+ *主模块，负责菜单的显示和处理用户操作
+ */
+public class CustomerView {
+    private CustomerList customerList = new CustomerList(10);
+
+
+}
