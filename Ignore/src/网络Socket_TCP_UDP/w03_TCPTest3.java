8871d9551e005542ccf51d47fb416a656404a600
wukaijun <1172917145@qq.com>
2022-06-13 21:03:49 +0800

    传输 tcp

--- Ignore/src/网络Socket_TCP_UDP/w03_TCPTest3.java
+++ Ignore/src/网络Socket_TCP_UDP/w03_TCPTest3.java
@@ -1,24 +1,19 @@
 package src.网络Socket_TCP_UDP;
 
-/**
- * @Time : 2022/6/13 20:34
- * @Author : wu
- * @File : w03_TCPTest2.java
- * @Software: IntelliJ IDEA
- */
-
 import org.junit.Test;
 
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
-/*
-客户端发送文件给服务端，服务端将文件保存到本地
+/**
+ * @Time : 2022/6/13 20:41
+ * @Author : wu
+ * @File : w03_TCPTest3.java
+ * @Software: IntelliJ IDEA
  */
-public class w03_TCPTest2 {
+public class w03_TCPTest3 {
 
     @Test
     public void client() throws IOException {
@@ -35,9 +30,25 @@ public class w03_TCPTest2 {
             outputStream.write(buffer, 0, len);
         }
 
+        socket.shutdownOutput();
+
+        //接收来自于服务器端的数据，并显示到控制台上
+        InputStream inputStream = socket.getInputStream();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buffer1 = new byte[1024];
+        int len1;
+        while ((len1= inputStream.read(buffer1))!= -1){
+            byteArrayOutputStream.write(buffer1,0,len1);
+        }
+
+        System.out.println(byteArrayOutputStream);
+
+        //
         fileInputStream.close();
         outputStream.close();
         socket.close();
+        byteArrayOutputStream.close();
+
 
     }
 
@@ -48,19 +59,29 @@ public class w03_TCPTest2 {
             Socket socket = serverSocket.accept();
             InputStream inputStream = socket.getInputStream();
 
-            FileOutputStream fileOutputStream = new FileOutputStream("beautiful.PNG");
+            FileOutputStream fileOutputStream = new FileOutputStream("beautifu2.PNG");
             byte[] buffer = new byte[1024];
             int len;
             while ((len = inputStream.read(buffer)) != -1) {
                 fileOutputStream.write(buffer, 0, len);
             }
+
+            System.out.println("图片传输完成");
+
+            //服务器端给予客户端反馈
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("你好，非常好，我非常满意！".getBytes());
+
             System.out.println("收到了来自于：" + socket.getInetAddress().getHostAddress() + "的数据");
 
             fileOutputStream.close();
             inputStream.close();
             socket.close();
             serverSocket.close();
+            outputStream.close();
 
         }
+
+
     }
 }
