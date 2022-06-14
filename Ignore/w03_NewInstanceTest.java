b2d652cd5c8988e6241302a93296c99b61a8999e
wukaijun1996 <87848113+wukaijun1996@users.noreply.github.com>
2022-06-14 20:00:16 +0800

    Create w03_NewInstanceTest.java

---
+++ Ignore/w03_NewInstanceTest.java
@@ -0,0 +1,61 @@
+import org.junit.Test;
+
+import java.util.Random;
+
+/*
+
+通过反射创建对应的运行时类的对象
+
+ */
+public class w03_NewInstanceTest {
+
+    @Test
+    public void test1() throws InstantiationException, IllegalAccessException {
+
+        Class<Person> personClass = Person.class;
+        //newInstance() 调用此方法创建对应的运行时类的对象
+        Person person = personClass.newInstance();
+        System.out.println(person);
+
+    }
+
+    @Test
+    public void test2(){
+
+        for (int j = 0; j < 100; j++) {
+
+
+        int i = new Random().nextInt(3);
+        String classPath = "";
+        switch (i){
+            case 0:
+                classPath = "java.util.Date";
+                break;
+            case 1:
+                classPath = "java.lang.object";
+                break;
+            case 2:
+                classPath = "Person";
+                break;
+        }
+
+        try {
+            Object o = getInstance(classPath);
+            System.out.println(o);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        }
+
+    }
+
+    //创建一个指定类的对象
+    public Object getInstance(String classPath) throws Exception {
+        Class<?> aClass = Class.forName(classPath);
+        return aClass.newInstance();
+    }
+
+
+
+}
